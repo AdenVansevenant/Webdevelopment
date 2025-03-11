175bef64eaed985526e713aeecb3f4a70cdac2fd
AdenVansevenant <r1047514@student.vives.be>
2025-03-11 16:58:53 +0100

    laatste foutjes eruit + validator
    KLAAARRRR :)))

--- Labo5Java/oplossing formvalidatie eerste aanzet(1)/scripts/code.js
+++ Labo5Java/oplossing formvalidatie eerste aanzet(1)/scripts/code.js
@@ -16,10 +16,10 @@ const valideerVoornaam = () => {
 	let errVoornaam = document.getElementById("errVoornaam");
 	let voornaam = txtVoornaam.value.trim();
 	if (voornaam.length > 30) {
-		txtVoornaam.className = "invalid";
+		txtVoornaam.className = "invalid"; // invalid class instellen
 		errVoornaam.innerHTML = "max. 30 karakters";
 	} else {
-		txtVoornaam.className = "";
+		txtVoornaam.className = ""; // alle classes verwijderen
 		errVoornaam.innerHTML = "";
 	}
 };
@@ -44,11 +44,11 @@ const valideerGeboortedatum = () => {
 	let txtGeboortedatum = document.getElementById("txtGeboortedatum");
 	let errGeboortedatum = document.getElementById("errGeboortedatum");
 	let geboortedatum = txtGeboortedatum.value.trim();
-	const geboortedatumvalidatie = /^\d{4}-\d{2}-\d{2}$/;  //jjjj-mm-dd
+	const geboortedatumRegex = /^\d{4}-\d{2}-\d{2}$/; // Formaat jjjj-mm-dd
 	if (geboortedatum === "") {
 		txtGeboortedatum.className = "invalid";
 		errGeboortedatum.innerHTML = "verplicht veld";
-	} else if (!geboortedatumvalidatie.test(geboortedatum)) {
+	} else if (!geboortedatumRegex.test(geboortedatum)) {
 		txtGeboortedatum.className = "invalid";
 		errGeboortedatum.innerHTML = "formaat is niet jjjj-mm-dd";
 	} else {
@@ -61,11 +61,11 @@ const valideerEmail = () => {
 	let txtEmail = document.getElementById("txtEmail");
 	let errEmail = document.getElementById("errEmail");
 	let email = txtEmail.value.trim();
-	const emailvalidatie = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/; // E-mail validatie
+	const emailRegex = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/; // E-mail validatie
 	if (email === "") {
 		txtEmail.className = "invalid";
 		errEmail.innerHTML = "verplicht veld";
-	} else if (!emailvalidatie.test(email)) {
+	} else if (!emailRegex.test(email)) {
 		txtEmail.className = "invalid";
 		errEmail.innerHTML = "geen geldig email adres";
 	} else {
@@ -78,10 +78,16 @@ const valideerAantalKinderen = () => {
 	let txtAantalKinderen = document.getElementById("txtAantalKinderen");
 	let errAantalKinderen = document.getElementById("errAantalKinderen");
 	let aantalKinderen = txtAantalKinderen.value.trim();
-	if (!isGetal(aantalKinderen) || aantalKinderen < 0 || aantalKinderen >= 99) {
+	if (!isGetal(aantalKinderen) || aantalKinderen < 0) {
 		txtAantalKinderen.className = "invalid";
 		errAantalKinderen.innerHTML = "is geen positief getal";
-	} else {
+	}
+	else if (aantalKinderen >= 99)
+	{
+		txtAantalKinderen.className = "invalid";
+		errAantalKinderen.innerHTML = "is te vruchtbaar";
+	}
+	else {
 		txtAantalKinderen.className = "";
 		errAantalKinderen.innerHTML = "";
 	}
